14268964637a50522ebaa108618d3a33ec006ce7
brianonchieku <brianonchikeku0@gmail.com>
2025-07-21 23:31:21 +0300

    authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:chatapp/auth/login_or_register.dart';
+import 'package:chatapp/firebase_options.dart';
 import 'package:chatapp/pages/login_page.dart';
 import 'package:chatapp/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized()
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
