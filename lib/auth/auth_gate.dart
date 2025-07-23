d41be50942185c8134ef647bd8ff2c97e0f06ba8
brianonchieku <brianonchikeku0@gmail.com>
2025-07-23 23:38:22 +0300

    authentication

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,22 @@
+import 'package:chatapp/auth/login_or_register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return HomePage();
+          } else{
+            return LoginOrRegister();
+          }
+        }
+    );
+  }
+}
