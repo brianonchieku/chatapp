ab544a11b52edadc8fbfbe5914727e574a75d07f
brianonchieku <brianonchikeku0@gmail.com>
2025-07-25 16:17:55 +0300

    firestore

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,9 @@ import 'package:chatapp/services/auth/auth_service.dart';
 import 'package:chatapp/services/chat/chart_service.dart';
 import 'package:flutter/material.dart';
 
+import '../components/user_tile.dart';
+import 'chat_page.dart';
+
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -18,6 +21,45 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       drawer: Drawer(),
+      body: buildUserList(),
+    );
+  }
+
+  Widget buildUserList(){
+    return StreamBuilder(
+        stream: chatService.getUserStream(),
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Text('Error');
+          }
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.map<Widget>((userData) => buildUserListItem(userData, context)).toList(),
+          );
+
+        }
+    );
+  }
+
+  Widget buildUserListItem(Map<String, dynamic> userData, BuildContext context){
+    //display all users except current user
+    return UserTile(
+      text: userData['email'],
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ChatPage(
+              receiverEmail: userData['email'],
+            )
+        )
+
+        );
+      },
     );
   }
 }
+
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,14 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService{
   //initialize auth
   final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   //sign in
   Future<UserCredential> signIn(String email, String password) async{
     try{
       UserCredential userCredential = await auth.signInWithEmailAndPassword(email: email, password: password);
+      //create document for user if it doesn't exist
+      firestore.collection("users").doc(userCredential.user!.uid).set(
+          {
+            "uid": userCredential.user!.uid,
+            "email": email,
+            "createdAt": DateTime.now()
+          }
+      );
       return userCredential;
     } on FirebaseAuthException catch(e){
       throw Exception(e.code);
@@ -19,6 +29,16 @@ class AuthService{
   Future<UserCredential> signUp(String email, String password) async{
     try{
       UserCredential userCredential = await auth.createUserWithEmailAndPassword(email: email, password: password);
+
+      //create document for user
+      firestore.collection("users").doc(userCredential.user!.uid).set(
+        {
+          "uid": userCredential.user!.uid,
+          "email": email,
+          "createdAt": DateTime.now()
+        }
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e){
       throw Exception(e.code);
