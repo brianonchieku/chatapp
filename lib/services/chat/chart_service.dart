da054aa9836c41a2ca53c228461cb096c5722833
brianonchieku <brianonchikeku0@gmail.com>
2025-07-26 23:42:04 +0300

    chat room

--- lib/services/chat/chart_service.dart
+++ lib/services/chat/chart_service.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -19,17 +20,37 @@ class ChatService{
 
 
   //send messages
-  Future<void> sendMessage(String receiverId, message) async{
+  Future<void> sendMessage(String receiverID, message) async{
     //get current user info
     final String currentUserID = auth.currentUser!.uid;
     final String currentUserEmail = auth.currentUser!.email!;
     final Timestamp timestamp = Timestamp.now();
 
     //create new message
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp
+    );
 
+    //construct chatroom id for the two users
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); //sort the ids(this ensures the chatroomid is the same for any two people)
+    String chatroomID = ids.join("_");
 
+    //add new message to database
+    await firestore.collection("chat_rooms").doc(chatroomID).collection("messages").add(newMessage.toMap());
   }
 
   //get messages
+  Stream<QuerySnapshot> getMessage(String userID, otherUserID){
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatroomID = ids.join("_");
+    
+    return firestore.collection("chat_rooms").doc(chatroomID).collection("messages").orderBy("timestamp", descending: false).snapshots();
+  }
 
 }
